06adf728b231685602a77cc4cb6a3a05ed800f51
clong <2956957982@qq.com>
2019-12-20 18:49:11 +0800

    首页修改

--- home/data-server/router/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const fs = require("fs");
-const router = express.Router();
-router.use('/', (req, res) => {
-  const data = fs.readFileSync('./file/file.md');
-  res.json({data: data.toString()})
-});
-
-module.exports = router;
